eaef45a722d86301f68ae4d73330dd870d7fa1a4
yinyong <yanni4night@gmail.com>
2014-11-28 18:07:15 +0800

    grunt plugin easy loading

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,9 @@
 
 module.exports = function(grunt) {
 
+    require('time-grunt')(grunt);
+    require('load-grunt-tasks')(grunt);
+
     // Project configuration.
     grunt.initConfig({
         jshint: {
@@ -85,11 +88,6 @@ module.exports = function(grunt) {
 
     // Actually load this plugin's task(s).
     grunt.loadTasks('tasks');
-
-    // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
     // Whenever the "test" task is run, first clean the "tmp" dir, then run this
     // plugin's task(s), then test the result.
