fe450976f0c8a5233bfe771694df549f5f86461c
yinyong <yanni4night@gmail.com>
2014-11-28 14:38:47 +0800

    add django supported

--- tasks/web_swig.js
+++ tasks/web_swig.js
@@ -7,53 +7,70 @@
  */
 
 'use strict';
-var swig = require('swig');
 
 module.exports = function(grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
-  grunt.registerMultiTask('web_swig', 'Compile swig templates and json to htmls', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      swigOptions: {
-        cache: false
-      },
-      getData: function(file) {
-        return {};
-      }
-    });
+    // Please see the Grunt documentation for more information regarding task
+    // creation: http://gruntjs.com/creating-tasks
+    grunt.registerMultiTask('web_swig',
+        'Compile swig/django templates and json to htmls',
+        function() {
+            // Merge task-specific and/or target-specific options with these defaults.
+            var options = this.options({
+                swigOptions: {
+                    cache: false
+                },
+                getData: function(file) {
+                    return {};
+                },
+                useDjango: false
+            });
 
-    //Set swig options
-    swig.setDefaults(options.swigOptions);
+            var engine = options.useDjango ? require('django') :
+                require('swig');
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var mock, srcContent, src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      });
+            if (options.useDjango) {
+                engine.congfiure(options.swigOptions);
+            } else {
+                engine.setDefaults(options.swigOptions);
+            }
 
-      if (!src.length) {
-        grunt.log.warn('Source file not found.');
-        return;
-      } else if (src.length > 1) {
-        grunt.log.warn('Only the first source file will be compiled.');
-      }
+            // Iterate over all specified file groups.
+            this.files.forEach(function(f) {
+                // Concat specified files.
+                var mock, srcContent, src = f.src.filter(
+                    function(filepath) {
+                        // Warn on and remove invalid source files (if nonull was set).
+                        if (!grunt.file.exists(filepath)) {
+                            grunt.log.warn('Source file "' +
+                                filepath +
+                                '" not found.');
+                            return false;
+                        } else {
+                            return true;
+                        }
+                    });
 
-      mock = 'function' === typeof options.getData ? options.getData(src[0]) : (options.getData || {});
-      // Write the destination file.
-      grunt.file.write(f.dest, swig.renderFile(src[0], mock));
+                if (!src.length) {
+                    grunt.log.warn('Source file not found.');
+                    return;
+                } else if (src.length > 1) {
+                    grunt.log.warn(
+                        'Only the first source file will be compiled.'
+                    );
+                }
 
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
-  });
+                mock = 'function' === typeof options.getData ?
+                    options.getData(src[0]) : (options.getData || {});
+                // Write the destination file.
+                grunt.file.write(f.dest, engine.renderFile(src[
+                        0],
+                    mock));
+
+                // Print a success message.
+                grunt.log.writeln('File "' + f.dest +
+                    '" created.');
+            });
+        });
 
 };
